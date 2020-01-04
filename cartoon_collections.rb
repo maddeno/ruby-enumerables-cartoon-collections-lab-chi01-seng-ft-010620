f25efdf1d864eb1740864bc8bb7a309374e446de
Olivia Madden <madden.livi@gmail.com>
2020-01-04 08:19:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,17 +4,20 @@ def roll_call_dwarves(dwarves)
    end
 end
 
+
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do
     |call| call = "#{call.capitalize}!"
   end
 end
 
+
 def long_planeteer_calls(calls)
   calls.any? do
     |call| call.length > 4
   end
 end
+
 
 def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
